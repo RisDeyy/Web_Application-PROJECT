bb9d3fe2836d80f9bf86588fa3f626d13cb3703f
nghĩa <93433772+ngssdwrwe@users.noreply.github.com>
2023-07-26 15:18:39 +0700

    pạt 3

--- customuser/user_main.js
+++ customuser/user_main.js
@@ -6,3 +6,27 @@ const editIcons = document.querySelectorAll('.edit-icon');
                         alert('Edit data here');
                     });
                 });
+                function chooseFromComputer() {
+                    document.getElementById('profilePicture').click();
+                }
+            
+                function chooseFromURL() {
+                    var imageURL = prompt('Please enter the URL of the image:');
+                    if (imageURL) {
+                        var profileImage = document.getElementById('profileImage');
+                        profileImage.src = imageURL;
+                    }
+                }
+            
+                // Function to handle when an image is selected from the computer
+                document.getElementById('profilePicture').addEventListener('change', function(event) {
+                    var file = event.target.files[0];
+                    var reader = new FileReader();
+                    
+                    reader.onload = function(e) {
+                        var profileImage = document.getElementById('profileImage');
+                        profileImage.src = e.target.result;
+                    };
+                    
+                    reader.readAsDataURL(file);
+                });             
