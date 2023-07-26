d3a650ff4de34e3e7ede20b54f07a23596f6793c
nghĩa <93433772+ngssdwrwe@users.noreply.github.com>
2023-07-26 13:47:29 +0700

    tạm xong

---
+++ customuser/user_main.js
@@ -0,0 +1,8 @@
+const editIcons = document.querySelectorAll('.edit-icon');
+            
+                editIcons.forEach(icon => {
+                    icon.addEventListener('click', () => {
+                      
+                        alert('Edit data here');
+                    });
+                });
