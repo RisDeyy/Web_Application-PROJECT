07385f1592d0569f00e9ec46bcfa26aefb164e1d
nguyenthephung <136545799+nguyenthephung@users.noreply.github.com>
2023-09-02 11:36:51 +0700

    assd
    awddd

--- JWT-Tutorial-main/backend/routes/revenue-route.js
+++
@@ -1,6 +0,0 @@
-const revenue = require("../controllers/revenue");
-const router = require("express").Router();
-
-router.get("/revenue", revenue.getRevenue);
-router.get("chart",revenue.getChart)
-module.exports = router;
